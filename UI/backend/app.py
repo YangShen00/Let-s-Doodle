6e08060313cd10714e13eec3cd6185a2d98601ae
Yang Shen <ys656@cornell.edu>
2022-03-28 10:46:57 -0400

    tested api

--- UI/backend/app.py
+++ UI/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import numpy as np
 
@@ -8,12 +8,18 @@ app.config['TESTING'] = False
 
 CORS(app, support_credentials=True)
 
+
 @app.route('/evaluate', methods=['POST'])
 @cross_origin(supports_credentials=True)
 def getEvaluation():
 
-    input_json = request.get_json(force=True)
+    # input_json = request.get_json(force=True)
 
     result = np.random.choice([True, False])
+    print(result)
 
-    return result
+    return jsonify({"evaluation": bool(result)}), 200
+
+
+if __name__ == '__main__':
+    app.run(port=9000)
