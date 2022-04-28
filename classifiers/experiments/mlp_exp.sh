7469f37418a80631237ec7a912dd148c1104eb28
Linda Huang <sh837@cornell.edu>
2022-04-28 19:40:11 -0400

    changing default to save checkpoint

--- classifiers/experiments/mlp_exp.sh
+++ classifiers/experiments/mlp_exp.sh
@@ -5,6 +5,6 @@ num_layers_lst=(2 3)
 
 for num_layers in "${num_layers_lst[@]}"; do
     for hidden_channels in "${hidden_channels_lst[@]}"; do
-        python main.py --method mlp --num_layers $num_layers --hidden_channels $hidden_channels --display_step 25
+        python main.py --method mlp --num_layers $num_layers --hidden_channels $hidden_channels --display_step 25 --save_cp
     done
 done
