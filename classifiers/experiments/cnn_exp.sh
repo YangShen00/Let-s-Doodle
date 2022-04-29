8eb6c29e595e3637be9a1687342a38139cfff41b
Lu Cao <lc892@cornell.edu>
2022-04-29 11:27:58 -0400

    update cnn model

--- classifiers/experiments/cnn_exp.sh
+++ classifiers/experiments/cnn_exp.sh
@@ -3,5 +3,5 @@
 lr_lst=(0.001 0.002 0.003 0.004 0.005)
 
 for lr in "${lr_lst[@]}"; do
-    python3 main.py --method cnn --display_step 25--SGD --lr $lr
+    python3 main.py --method cnn --display_step 25 --SGD --lr $lr
 done
