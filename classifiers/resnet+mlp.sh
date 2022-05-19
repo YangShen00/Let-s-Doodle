1874a72a775e7c1ddd3ca171f1baf135224f692a
Linda Huang <sh837@cornell.edu>
2022-05-18 20:03:41 -0400

    hehe

--- classifiers/resnet+mlp.sh
+++ classifiers/resnet+mlp.sh
@@ -8,7 +8,7 @@
 #SBATCH -n 4  # Total number of CPU cores requrested
 #SBATCH -t 24:00:00  # Run time (hh:mm:ss)
 #SBATCH --mem=20000  # CPU Memory pool for all cores
-#SBATCH --partition=cuvl --gres=gpu:1
+#SBATCH --partition=default_partition --gres=gpu:1
 #SBATCH --get-user-env
 
 # Put the command you want to run here. For example:
